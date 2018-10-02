3b3d93e0ffd9b401657b19a7577b5912f3def3c8
Chris Zubak-Skees <chriszs@gmail.com>
2018-10-02 06:57:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Outside contribs graphics
+
+> 
+
+## Build setup
+
+``` bash
+# install dependencies
+$ yarn
+
+# serve with hot reload at localhost:3000/outside-contribs-graphics
+$ yarn dev
+
+# build for production and launch server
+$ yarn start
+
+# or generate static page
+$ yarn generate
+
+# and push to s3
+$ yarn push
+```
+
+For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
---
+++ nuxt.config.js
@@ -0,0 +1,104 @@
+const pkg = require('./package.json');
+
+module.exports = {
+    /*
+     ** Headers of the page
+     */
+    loading: false,
+    head: {
+        title: 'Outside contribs graphics',
+        meta: [
+            { charset: 'utf-8' },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: ''
+            }
+        ],
+        link: [
+            {
+                rel: 'icon',
+                type: 'image/x-icon',
+                href: `/${pkg.name}/favicon.ico`
+            }
+        ]
+    },
+    modules: [
+        '@nuxtjs/axios',
+        [
+            '@nuxtjs/google-analytics',
+            {
+                id: 'UA-3383794-4',
+                debug: {
+                    sendHitTask: !(
+                        typeof document !== 'undefined' &&
+                        document &&
+                        document.referrer &&
+                        document.referrer.indexOf('publicintegrity.org') !== -1
+                    )
+                }
+            }
+        ]
+    ],
+    render: {
+        ssr: false
+    },
+    plugins: [
+        { src: '~/plugins/pym.js', ssr: false },
+        { src: '~/plugins/typekit.js', ssr: false },
+        { src: '~/plugins/chartbeat.js', ssr: false }
+    ],
+    axios: {
+        host: process.env.HOST || 'localhost',
+        prefix: `/${pkg.name}/`,
+        port: process.env.PORT || 3000,
+        https: true
+    },
+    generate: {
+        minify: {
+            collapseWhitespace: false,
+            removeEmptyAttributes: false
+        }
+    },
+    router: {
+        base: `/${pkg.name}/`
+    },
+    /*
+     ** Global CSS
+     */
+    css: ['~/assets/css/site.css', '~/assets/css/main.css'],
+    /*
+     ** Add axios globally
+     */
+    build: {
+        /*
+         ** Run ESLINT on save
+         */
+        extend(config, ctx) {
+            /*            config.module.rules.push({
+                test: /\.(csv)$/,
+                loader: 'dsv-loader',
+                exclude: /(node_modules)/
+            });
+*/
+
+            config.module.rules.push({
+                test: /\.csv$/,
+                use: [{ loader: 'raw-loader' }]
+            });
+
+            if (ctx.isClient && process.env.NODE_ENV !== 'production') {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                });
+            }
+        }
+    }
+};
